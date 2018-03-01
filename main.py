5ce620311ba69f1d64b5936325bd176be7c2ebda
kszilagyi <kristof.szilagyi1@gmail.com>
2018-03-01 20:38:56 +0000

    Reformat file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 from slackclient import SlackClient
 from time import time
 from datetime import datetime
+
 parser = argparse.ArgumentParser(description='Lists people who has left a slack channel')
 
 parser.add_argument('--from-month-ago', type=int, required=True,
@@ -28,8 +29,8 @@ sc = SlackClient(slack_token)
 x = '%s' % channel_name
 print(x)
 
-
-channel_id = next((channel['id'] for channel in sc.api_call("channels.list")['channels'] if channel['name'] == channel_name), None)
+channel_id = next(
+    (channel['id'] for channel in sc.api_call("channels.list")['channels'] if channel['name'] == channel_name), None)
 if channel_id is None:
     print('channel not found')
     exit(1)
@@ -39,7 +40,6 @@ print("Channel id is %s" % channel_id)
 all_events = sc.api_call("channels.history",
                          channel=channel_id,
                          count=1000)
-# only_leaves = [event for event in all_events['messages'] if event['sub_type'] == 'channel_leave']
 only_leaves = []
 for event in all_events['messages']:
     if 'subtype' in event and event['subtype'] == 'channel_leave':
@@ -53,15 +53,16 @@ to_timestamp = time() - to_month_ago * rude_month_timestamp
 print("from: %s" % datetime.fromtimestamp(from_timestamp))
 print("to: %s" % datetime.fromtimestamp(to_timestamp))
 
-only_leaves_in_period = set([leave['user'] for leave in only_leaves if from_timestamp < float(leave['ts']) < to_timestamp])
+only_leaves_in_period = set(
+    [leave['user'] for leave in only_leaves if from_timestamp < float(leave['ts']) < to_timestamp])
 
 # todo this doesn't work if the number of people is bigger than 100
 current_members = set(sc.api_call('conversations.members',
-                              channel=channel_id)['members'])
+                                  channel=channel_id)['members'])
 left_and_didnt_come_back = only_leaves_in_period - current_members
 
 resolved_left_and_didnt_come_back = [sc.api_call("users.info",
-                                              user=user)['user']for user in left_and_didnt_come_back]
+                                                 user=user)['user'] for user in left_and_didnt_come_back]
 
 without_deleted = [user['profile']['email'] for user in resolved_left_and_didnt_come_back if not user['deleted']]
 
